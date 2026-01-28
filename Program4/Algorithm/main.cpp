74519736207199daa95c0b88a71d2feb0b16934c
0 <msw4761@naver.com>
2026-01-28 16:58:43 +0900

    DAY13 퀵 정렬 구현

--- Program4/Algorithm/main.cpp
+++ Program4/Algorithm/main.cpp
@@ -109,7 +109,7 @@ void Insertsort(vector<int>& v)
 // 머지, 힙, 퀵 sort 
 // greater - 작은 것 위, less 큰 것이 위
 
-// 
+ 
 void HeapSort(vector<int>& v)
 {
 	priority_queue<int, vector<int>, greater<int>> pq;
@@ -178,7 +178,7 @@ void MergeResult(vector<int>& v, int left, int mid, int right)
 		
 	// temp -> vector 넣어준다
 	for (int i = 0; i < temp.size(); i++)
-		v[left + 1] = temp[i];
+		v[left + i] = temp[i];			// + 1로 하여 정렬이 안됌. +i로 고쳐 정렬 가능
 
 }
 
@@ -195,6 +195,66 @@ void MergeSort(vector<int>& v, int left, int right)
 	MergeResult(v, left, mid, right);
 }
 
+// 힙, Merge, quick
+// quick - 
+// merge -> middle
+// 피벗 빠르게 선택했다. 
+
+// pivot
+
+// [5] | [1][3][7][9][2][4][6][8]
+// P	  l                    h	pivot 어느 위치에 있으면 가장 어울릴까?
+//		[1][3][4][9][2][7][6][8]
+//		[1][3][4][2][9][7][6][8]
+//		[2][1][3][4][5][9][7][6][8]
+//   
+// 1> low high low->	<-high	pivot값			<-[5]->
+// //  v[low] <= p	     v[high] >= p
+// 2> low indext값(2)  <  high(5)	swap
+// 3> swap(v[left], v[h])
+
+// Heap vs Merge vs Q [sort]
+// nlogn			n^2, nlogn
+// nlogn 시간을 갖는다. 
+// 대부분 퀵솔트로 시간적으로 별로임. 대신 공간적으로 효율 시간과 공간의 반비례, 따로 메모리 사용을 하지 않는다. 
+// Merge의 경우 temp를 사용하여 메모리 사용, Heap은 PQ 사용으로 메모리 사용. 시간 절약을 위해, 공간 사용.
+
+int Partition(vector<int>& v, int left, int right) // 퀵솔트의 핵심코드
+{
+	int pivot = v[left];
+	int low = left + 1;
+	int high = right;
+	
+	// pivot의 수에 따라 결과가 달라짐. 최하(pivot이 제일 작거나 제일 큰 경우) (N) - logN 까지 시간경과 범위 있음.
+	// 대략 n^2 ~ nlogN 시간 범위
+	while (low <= high)
+	{
+		while (low <= right && pivot >= v[low])			// 배열 범위 && 피벗 비교
+			low++;
+		while (high >= left + 1 && pivot <= v[high])	// 배열 범위 && 피벗 비교
+			high--;
+		if (low < high)
+			swap(v[low], v[high]);
+	}
+	
+	
+	swap(v[left], v[high]);
+	return high;										// 피벗이 위치해야할 index값
+}
+
+void QuickSort(vector<int>& v, int left, int right)
+{
+	if (left > right)
+		return;
+
+	int pivot = Partition(v, left, right);
+	QuickSort(v, left, pivot - 1);						// 왼쪽 파트 재귀
+	QuickSort(v, pivot + 1, right);						// 오른쪽 파트 재귀
+
+}
+
+// 탐색. N 전수 조사는 N시간이 걸린다. 
+// nlogn * logn = nlog(n제곱)
 int main()
 {
 	vector<int> v{ 4,1,3,2,5 };
@@ -205,8 +265,17 @@ int main()
 	//BubbleSort(v);
 	//selectSort(v);
 	//Insertsort(v);
-	// HeapSort(v);
-	MergeSort(v, 0, v.size() - 1);
+	//HeapSort(v);
+	//MergeSort(v, 0, v.size() - 1);
+	QuickSort(v, 0, v.size() - 1); // [1] 1 0 
+
+	// 연관 컨테이너 - 탐색
+	// 보통 logN 시간 걸림.
+	// 추가. logN <<<< 로딩. 
+	//				실제 게임에서의 렉
+	// 
+	// 탐색 시간이 빠르다. logN이 걸림. < nlogN
+
 	for(const auto & a : v)
 	{
 		cout << a << " ";
