d1d1aa574543a7708704d39a26140dc1ecf85d89
0 <msw4761@naver.com>
2026-01-29 17:51:10 +0900

    DAY14 Hash Table 사용 샘플 예제

--- Program4/Algorithm/main.cpp
+++ Program4/Algorithm/main.cpp
@@ -10,135 +10,136 @@
 #include <unordered_map>
 #include <algorithm>
 #include <Windows.h>
+#include <memory.h>
 
 using namespace std;
 
-// Hash Table
+// new - delete => 
 
-// 비용. BFS -> mindistance N
-// sort() -> nlogn
-// PQ -> O(1) - logn
+template <typename T>
+using Uptr = std::unique_ptr<T>;		//	치환 패턴
 
-// [0] vector
-// [1] map vs hash table
-// map -> logn
+enum class ItemType {WEAPON, ARMOR, CONSUMABLE};
+enum class WeaponType {SWORD, BOW};
+enum class ArmorType {HELMET, ARMOR};
+enum class consumableType {POTION};
 
-// table - 시간을 위해 공간을 사용했다. 
-// 
-
-void Table()
+// base 클래스. 
+// ~소멸자 base에서 virtual 선언.
+class Item
 {
-	struct User
-	{
-		int id = 0;
-		int Value;
-	};
+public:
+	Item(ItemType type, int id, string name) : _type(type), _id(id), _name(name) {} // ctor입력, 탭을 누르면 단축키 자동완성
+	virtual ~Item() = default;
 
-	vector<User> users;		 
-	users.resize(1'000);
+	virtual void Use() {}
 
-	users[999] = User{ 999, 999 * 10 };
+public:
+	ItemType _type;
+	int _id;
+	string _name;
+};
 
-	// Q 999번 아이디의 유저가 가지고 있는 값을 출력하세요. 
-
-	cout << "유저가 가지고 있는 값 " << users[999].Value << endl;
-
-
-}
-
-// hash
-
-void Hash()
+class WeaponItem : public Item
 {
-	struct User
+public:
+	WeaponItem(ItemType type, int id, string name, WeaponType wtype, int damage)
+		: Item{ type, id, name }, _wtype(wtype), _damage(damage) 
 	{
-		int id = 0;
-		string name;
-	};
-
-	vector<User> users;
-
-	users.resize(1000);
-
-	int userId = 123'456'789;
-	int key = (userId % 1000); // 0 ~ 999 = 1000개
-
-	users[key] = User{ userId, "AAA"};
-	
-	int user2Id = 789;
-	int key2 = (user2Id % 1000);
-
-	users[key] = User{ user2Id, "BBB" };
-
-	// 숫자가 너무 크다 -> 모두 저장x -> 쪼개겠다. bucket [			]
-
-	// 해결하기 위한 방법들을 생각을 했어야 한다. 
-	// 이미 데이터가 들어 있다? 넣지 않고 + 1 그 다음에 자리가 있나? 선형 조사법
-	// hash(key) + 1^2											 이차 조사법
-
-	// 체이닝 : 중복된 데이터가 있으면? 연속적으로 데이터를 이어가겠다. 
-}
-
-// [789, "AAA"] -> [789, "BBB"]
-// 이 데이터들 중에서 아이디가 AAA인 녀석을 찾아라.
-
-// 함수 -> 중복이 안되도록 하는 엄청나게 좋은 코드를 추구
-
-void HashChaining()
-{
-	struct User
-	{
-		int id = 0;
-		string name;
-	};
-
-	vector<vector<User>> users;
-
-	users.resize(1000);
-
-	int userId = 123'456'789;
-	int key = (userId % 1000); // 0 ~ 999 = 1000개
-	int user2Id = 789;
-	int key2 = (user2Id % 1000);
-
-	users[key].push_back(User{ userId, "AAA" });
-	users[key2].push_back(User{ user2Id, "BBB" });
-
-	for (User& user : users[key])
-	{
-		if (user.id == userId)
-		{
-			cout << user.name << endl;
-		}
+		type = ItemType::WEAPON;
 	}
 
-}
+	void Use() override
+	{
+		// 아이템 장착(Equip)
+		// 무기를 휘둘렀다. 
+		cout << "[" << _name <<"] 사용했다." << "공격력 : " << _damage << endl;
+	}
 
-// map -> O(n)
-// Hash Table -> 탐색 시간 O(1)
-// 공간을 사용해서 시간을 매우 추구한 버전 map보다 더
+public:
+	WeaponType _wtype;
+	int _damage;
+};
 
-// 로딩...데이터 초기화
-// 게임 플레이 => Database 99 -> 123 -> 10
-// 99 <- insert [10]
-// n << 아이템 업데이트. 1만. << 게임 렉이 걸림. O(n)일 경우 굉장히 비효율적.
-// map< >	-	hash table< > STL Cpp == unordered_map, C# == Dictionary 둘다 해쉬 테이블(해쉬 기술 + 테이블 기술), 언어에 따라 다르게 불림.
-// 
-// 이름과 추구하는 목적성, 로직의 원리를 아는 것이 중요하다.
-// 
+class ArmorItem : public Item
+{
+public:
+	ArmorItem(ItemType type, int id, string name, WeaponType wtype, int defence)
+		: Item{ id, name }, _atype(atype), _defence(defence) 
+	{
+		type = ItemType::WEAPON;
+	}
 
-// vector, map, hash table << data table;
+	void Use() override
+	{
+		// 아이템 장착(Equip)
+		// 무기를 휘둘렀다. 
+		cout << "[" << _name << "] 사용했다." << "공격력 : " << _defence << endl;
+	}
+
+public:
+	ArmorType _atype;
+	int _defence;
+};
+
+class ConsumableItem : public Item
+{
+public:
+	consumableItem(ItemType type, int id, string name, WeaponType wtype, int stack)
+		: Item{ type, id, name }, _ctype(ctype), _stack(_stack) 
+	{}
+
+	void Use() override
+	{
+		// 아이템 장착(Equip)
+		// 무기를 휘둘렀다. 
+		cout << "[" << _name << "] 사용했다." << "공격력 : " << _stack << endl;
+	}
+
+public:
+	ConsumableType _ctype;
+	int _stack;
+};
 
 int main()
 {
-	//Table();
-	//Hash();
-	//HashChaining();
+	srand(time(0));
+#pragma region Item
+	unordered_map<int, Uptr<Item>> itemDict;
 
-	unordered_map<int, string> users;
+	vector<Uptr<WeaponItem>> weapons;
+	vector<Uptr<ArmorItem>> armors;
+	vector<Uptr<ConsumableItem>> consumables;
 
-	users.insert(make_pair(123'456'789, "AAA"));
-	users.insert(make_pair(789, "BBB"));
+	weapons.push_back(make_unique<WeaponItem>(1, "검", WeaponType::SWORD, 10));
+	weapons.push_back(make_unique<WeaponItem>(2, "활", WeaponType::BOW, 5));
 
-	cout << users[789] << endl;
+	armors.push_back(make_unique<ArmorItem>(100, "투구", ArmorType::HELMET, 2));
+	armors.push_back(make_unique<ArmorItem>(101, "갑옷", ArmorType::ARMOR, 3));
+
+	consumables.push_back(make_unique<ArmorItem>(200, "갑옷", consumableType::POTION, 1));
+
+
+	for (auto& w : weapons)
+		itemDict.insert({ w->_id, std::move(w) });
+	for (auto& a : armors)
+		itemDict.insert({ a->_id, std::move(a) });
+	for (auto& c : consumables)
+		itemDict.insert({ c->_id, std::move(c) });
+
+#pragma 
+
+	if (!itemDict.empty())
+	{
+		int randValue = rand() % itemDict.size();
+		auto it = itemDict.begin();
+
+		advance(it, randValue);		//		iterator 반복문 버전.
+
+		Item* itemTemp = it->second.get();
+
+		cout << "아이템 획득: " << itemTemp->_name << endl;
+
+		itemTemp->Use();
+	}
 }
